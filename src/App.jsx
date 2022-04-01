a46cc9a19c833e88c14444e6951d480895b43475
wickedSharma <61442144+abhijitdotsharma@users.noreply.github.com>
2022-04-01 21:17:58 +0530

    route for mockman added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import {Routes, Route} from "react-router-dom";
-import {LandingPage, ProductPage} from "./pages/"
+import {LandingPage, ProductPage} from "./pages/";
+import Mockman from "mockman-js";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/products" element={<ProductPage />}/>
+        <Route path="/mock" element={<Mockman/>} />
       </Routes>
     </div>
   );
