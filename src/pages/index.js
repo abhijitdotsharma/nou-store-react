3a2c1fa084c4666a78d869263e5bcd6c2376a3fe
abhijitdotsharma <sharma.abhijit2050@gmail.com>
2022-07-06 15:36:45 +0530

    WishlistPage exported

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import CartPage  from "./cart-page/CartPage";
 import LandingPage from "./landing-page/LandingPage";
 import ProductPage from "./product-page/ProductPage";
+import WishlistPage from "./wishlist-page/WishlistPage";
 
-export {CartPage, LandingPage, ProductPage};
+export {CartPage, LandingPage, ProductPage, WishlistPage};
