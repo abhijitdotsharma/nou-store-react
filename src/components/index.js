7ca719bcf68ce74f9b49af8b5b2cec5c29f38522
abhijitdotsharma <sharma.abhijit2050@gmail.com>
2022-07-06 15:33:24 +0530

    minor - <Wishlist> ,  <WishlistCard/> & 1 utility function exported from components\index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,16 @@
 import CartProduct from "./cart/CartProduct";
 import { CartProducts } from "./cart/CartProducts";
 import Categories from "./categories/Categories";
-import Filter from "./filter/Filter"
+import Filter from "./filter/Filter";
 import HeroSection from "./hero-section/HeroSection";
 import NavigationTop from "./navigation-top/NavigationTop";
 import NewArrival from "./new-arrival/NewArrival";
 import Products from "./products/Products";
+import WishlistCard from "./wishlists/WishlistCard";
+import Wishlists from "./wishlists/Wishlists";
 
-export {CartProduct,CartProducts, Categories, Filter, HeroSection, NavigationTop, NewArrival, Products};
+//utilities
+import { getDiscountOfProduct } from "./utilities/getDiscountOfProduct";
+
+
+export { CartProduct, CartProducts, Categories, Filter, getDiscountOfProduct, HeroSection, NavigationTop, NewArrival, Products, Wishlists, WishlistCard };
