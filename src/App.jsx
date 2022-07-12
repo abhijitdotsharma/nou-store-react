d6e6fbd2bebb4a827c9d649e5f7f53801e11bdd8
abhijitdotsharma <sharma.abhijit2050@gmail.com>
2022-07-12 14:55:11 +0530

    Protected Routes to Cart and Wishlist in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import "./App.css";
 import {Routes, Route} from "react-router-dom";
-import {CartPage, LandingPage, ProductPage, WishlistPage} from "./pages/";
+import {CartPage, LandingPage, LoginPage, ProductPage, SignupPage, WishlistPage} from "./pages/";
 import Mockman from "mockman-js";
+import RequiresAuth from "./components/RequiresAuth";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/products" element={<ProductPage />}/>
-        <Route path="/cart" element={<CartPage/>} />
-        <Route path="/wishlist" element={<WishlistPage/>} />
+        
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage/>} />
         <Route path="/mock" element={<Mockman/>} />
+
+        {/* private routes */}
+        <Route path="/cart" element={
+          <RequiresAuth>
+            <CartPage/>
+          </RequiresAuth>
+        } />
+
+        <Route path="/wishlist" element={
+          <RequiresAuth>
+            <WishlistPage/>
+          </RequiresAuth>
+        } />
+
       </Routes>
     </div>
   );
