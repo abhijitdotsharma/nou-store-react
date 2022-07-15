a8e1544b0947c7abc86cb0a3bafd23131fb273c1
abhijitdotsharma <sharma.abhijit2050@gmail.com>
2022-07-15 11:37:26 +0530

    NewArrival updated with media query

--- src/components/new-arrival/NewArrival.jsx
+++ src/components/new-arrival/NewArrival.jsx
@@ -3,26 +3,20 @@ import "./new-arrival.css";
 
 export default function NewArrival() {
     return (
-        <section className="new-arrival-section">
-            <div className="new-arrival-card">
-                <div className="new-arrival-image">
-                    <img src="https://picsum.photos/500" alt="New Arrival" />
-                </div>
-                <div className="new-arrival-content">
-                    <span>Recently Launched</span>
-                    <h3>Spring 2022 Collection</h3>
-                    <p>Our latest collection of on and
-                        off-the-field merch.
-                    </p>
-                    <Link
-                        to="/products"
-                        className="new-arrival-btn"
-                    >Check it out</Link>
-
-                </div>
+        <section className="new-arrival-card">
+            <div className="new-arrival-image">
+                <img src="https://picsum.photos/900" alt="New Arrival" />
             </div>
-            <div>
-
+            <div className="new-arrival-content">
+                <span>Recently Launched</span>
+                <h3>Spring 2022 Collection</h3>
+                <p>Our latest collection of on and
+                    off-the-field merch.
+                </p>
+                <Link
+                    to="/products"
+                    className="new-arrival-btn"
+                >Check it out</Link>
             </div>
         </section>
     )
