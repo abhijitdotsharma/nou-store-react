6cbe72e85a70921ba2aa471eff0a20401cbb52f5
abhijitdotsharma <sharma.abhijit2050@gmail.com>
2022-07-29 21:25:42 +0530

    landing-page Categories and NewArrival changes

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -8,22 +8,30 @@ export default function Categories() {
             <div className="category-images">
                 <div className="category-image">
                     <Link to="/products">
-                        <img src="https://picsum.photos/200" alt="category 1" />
+                        {/* <img src="https://res.cloudinary.com/wickedsharma/image/upload/v1658942612/nou-store/jacket-landing_rmb3mf.jpg" alt="category 1" /> */}
+                        <div className="category-bg-image category-image__one"></div>
+                        <h3 className="category-image__text">Jackets</h3>
                     </Link>
                 </div>
                 <div className="category-image">
                     <Link to="/products">
-                        <img src="https://picsum.photos/200" alt="category 1" />
+                        {/* <img src="https://res.cloudinary.com/wickedsharma/image/upload/v1658943288/nou-store/landing-jersey_b7mypg.jpg" alt="category 1" /> */}
+                        <div className="category-bg-image category-image__two"></div>
+                        <h3 className="category-image__text">Jerseys</h3>
                     </Link>
                 </div>
                 <div className="category-image">
                     <Link to="/products">
-                        <img src="https://picsum.photos/200" alt="category 1" />
+                        {/* <img src="https://res.cloudinary.com/wickedsharma/image/upload/v1658943437/nou-store/landing-boots_yfqkys.jpg" alt="category 1" /> */}
+                        <div className="category-bg-image category-image__three"></div>
+                        <h3 className="category-image__text">Boots</h3>
                     </Link>
                 </div>
                 <div className="category-image">
                     <Link to="/products">
-                        <img src="https://picsum.photos/200" alt="category 1" />
+                        {/* <img src="https://res.cloudinary.com/wickedsharma/image/upload/v1658943577/nou-store/landing-football_t3iuan.jpg" alt="category 1" /> */}
+                        <div className="category-bg-image category-image__four"></div>
+                        <h3 className="category-image__text">Footballs</h3>
                     </Link>
                 </div>
             </div>
--- src/components/new-arrival/NewArrival.jsx
+++ src/components/new-arrival/NewArrival.jsx
@@ -5,19 +5,20 @@ export default function NewArrival() {
     return (
         <section className="new-arrival-card">
             <div className="new-arrival-image">
-                <img src="https://picsum.photos/900" alt="New Arrival" />
+                <img src="https://res.cloudinary.com/wickedsharma/image/upload/v1658940816/nou-store/babies-above-footer_tutzxo.jpg" alt="New Arrival" />
+                <div className="new-arrival-content">
+                    <span>Recently Launched</span>
+                    <h3>Spring 2022 Collection</h3>
+                    <p>Our latest collection of on and
+                        off-the-field merch.
+                    </p>
+                    <Link
+                        to="/products"
+                        className="new-arrival-btn"
+                    >Check it out</Link>
+                </div>
             </div>
-            <div className="new-arrival-content">
-                <span>Recently Launched</span>
-                <h3>Spring 2022 Collection</h3>
-                <p>Our latest collection of on and
-                    off-the-field merch.
-                </p>
-                <Link
-                    to="/products"
-                    className="new-arrival-btn"
-                >Check it out</Link>
-            </div>
+
         </section>
     )
 }
